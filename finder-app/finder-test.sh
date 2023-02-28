75ec150a7a6ce68bed63109e90defa35302946c4
jichao <fangdxc@gmail.com>
2023-02-28 10:35:52 -0600

    makefile

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -49,7 +49,7 @@ then
 	fi
 fi
 
-echo "Removing the old writer utility and compiling as a native application"
+#echo "Removing the old writer utility and compiling as a native application"
 #make clean
 #make
 
